67227906602a82ec7649dc8a2a7a16e0f1c43e0a
Nick Humrich <nick.humrich@gmail.com>
2016-12-05 10:29:18 -0700

    remove uneeded files

---
+++ examples/rabbit_foo.py
@@ -0,0 +1,29 @@
+
+from wasp import Application
+from wasp.transports import RabbitMQTransport
+
+app = Application(
+    RabbitMQTransport(
+
+    ),
+    debug=False)
+
+
+async def handle_hello(request):
+    return {'hello': 'world'}
+
+async def handle_foo(request):
+    fooid = request.path_params.get('fooid')
+    return {'foo': fooid}
+
+async def handle_bar(request):
+    fooid = request.path_params.get('fooid')
+    barid = request.query.get('bar')
+    return {'foo': fooid, 'bar': barid}
+
+app.router.add_static_route('get', '/hello', handle_hello)
+app.router.add_get('/foo/:fooid', handle_foo)
+app.router.add_get('/foo/:fooid/bar', handle_bar)
+
+if __name__ == '__main__':
+    app.run()
