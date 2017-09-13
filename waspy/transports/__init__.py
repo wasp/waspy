7dcf281eed0e63f4dbbfcdc392c46b793d17fa72
Nick Humrich <nick.humrich@gmail.com>
2017-09-13 15:29:53 -0600

    Fix imports

--- waspy/transports/__init__.py
+++ waspy/transports/__init__.py
@@ -1,6 +1,6 @@
 from .httptransport import HTTPTransport, HTTPClientTransport
 from .httptoolstransport import HTTPTransport as HTTPToolsTransport
-from .rabbitmqtransport import RabbitMQTransport
+from .rabbitmqtransport import RabbitMQTransport, RabbitMQClientTransport
 from .transportabc import TransportABC
 from .testtransport import TestTransport
 
