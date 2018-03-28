7ba8f86dff2aed05d6696b7db74aba2b1f10f819
Jared Mackey <jared@mackey.tech>
2018-03-28 11:50:52 -0600

    test parsing instead of getting the body again

--- waspy/app.py
+++ waspy/app.py
@@ -191,7 +191,7 @@ class Application:
                     exc_info = sys.exc_info()
                     self.logger.log_exception(request, exc_info, level='warning')
             # invoke serialization (json) to make sure it works
-            _ = response.body
+            _ = response.raw_body
 
         except CancelledError:
             # This error can happen if a client closes the connection
